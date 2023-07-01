5047bf78f749039d22100387ccf9471653adb648
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2023-07-01 20:48:08 -0300

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -27,16 +27,12 @@ def is_channel_live(url):
 def grab(url):
     response = requests.get(url, timeout=15).text
     if '.m3u8' not in response:
-        #response = requests.get(url).text
+        if windows:
+            return 'https://archive.org/download/video-x-ts-1920x-1080-dtd-4/Video%20x%20Ts_1920x1080_dtd-4.ia.mp4'
+        os.system(f'wget {url} -O temp.txt')
+        response = ''.join(open('temp.txt').readlines())
         if '.m3u8' not in response:
-            if windows:
-                print('https://archive.org/download/video-x-ts-1920x-1080-dtd-4/Video%20x%20Ts_1920x1080_dtd-4.ia.mp4')
-                return
-            os.system(f'wget {url} -O temp.txt')
-            response = ''.join(open('temp.txt').readlines())
-            if '.m3u8' not in response:
-                print('https://archive.org/download/blink182documentary2004/blink182lostdocumentary.ia.mp4')
-                return
+            return 'https://archive.org/download/blink182documentary2004/blink182lostdocumentary.ia.mp4'
     end = response.find('.m3u8') + 5
     tuner = 100
     while True:
@@ -47,29 +43,32 @@ def grab(url):
             break
         else:
             tuner += 5
-    print(f"{link[start : end]}")
+    return link[start : end]
 
-print('#EXTM3U x-tvg-url="https://github.com/botallen/epg/releases/download/latest/epg.xml"')
-print(banner)
-#s = requests.Session()
+output_file = 'output.m3u'  # Nome do arquivo de saída
+
 with open('../youtube_channel_info.txt', errors="ignore") as f:
-    for line in f:
-        line = line.strip()
-        if not line or line.startswith('~~'):
-            continue
-        if not line.startswith('https:'):
-            line = line.split('|')
-            ch_name = line[0].strip()
-            grp_title = line[1].strip().title()
-            tvg_logo = line[2].strip()
-            tvg_id = line[3].strip()
-            channel_url = line[4].strip()  # Assuming the channel URL is in the 5th position in the line
-            if is_channel_live(channel_url):
-                print(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}')
-        else:
-            grab(line)
+    with open(output_file, 'w') as output:
+        output.write('#EXTM3U x-tvg-url="https://github.com/botallen/epg/releases/download/latest/epg.xml"\n')
+        output.write(banner)
+        
+        for line in f:
+            line = line.strip()
+            if not line or line.startswith('~~'):
+                continue
+            if not line.startswith('https:'):
+                line = line.split('|')
+                ch_name = line[0].strip()
+                grp_title = line[1].strip().title()
+                tvg_logo = line[2].strip()
+                tvg_id = line[3].strip()
+                channel_url = line[4].strip()  # Assuming the channel URL is in the 5th position in the line
+                if is_channel_live(channel_url):
+                    output.write(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}\n')
+                    output.write(grab(channel_url) + '\n')
+            else:
+                output.write(grab(line) + '\n')
 
 if 'temp.txt' in os.listdir():
     os.system('rm temp.txt')
     os.system('rm watch*')
-    
