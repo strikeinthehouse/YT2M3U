cd5d35c02c683993c55fe77be382f78a8ad74fbe
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2023-07-01 21:15:53 -0300

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -47,7 +47,6 @@ print('#EXTM3U x-tvg-url="https://github.com/botallen/epg/releases/download/late
 print(banner)
 
 with open('../youtube_channel_info.txt', errors="ignore") as f:
-    is_online = False
     for line in f:
         line = line.strip()
         if not line or line.startswith('~~'):
@@ -55,26 +54,14 @@ with open('../youtube_channel_info.txt', errors="ignore") as f:
         if not line.startswith('https:'):
             line = line.split('|')
             ch_name = line[0].strip()
-            grp_title = line[1].strip().title()
+            grp_title = line[1].strip()
             tvg_logo = line[2].strip()
             tvg_id = line[3].strip()
-            is_online = False
+            print(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}', end='')
         else:
-            response = requests.get(line, timeout=15)
-            if response.status_code == 200:
-                is_online = True
-                if not line.startswith('https://'):
-                    line = 'https://' + line
-                grab(line)
-            else:
-                is_online = False
-        
-        if not is_online:
-            continue
-
-        if is_online:
-            print(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}')
+            grab(line)
 
 if 'temp.txt' in os.listdir():
     os.system('rm temp.txt')
     os.system('rm watch*')
+
